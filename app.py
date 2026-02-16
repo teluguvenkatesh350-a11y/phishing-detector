5625ecca2123a97ff1a2f274e3aeb771fb3f0667
T.G.Venkatesh <teluguvenkatesh350@gmail.com>
2026-02-17 02:02:53 +0530

    Change app run configuration
    Updated the app to run on host '0.0.0.0' and port 10000.

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def check():
 
     return render_template("result.html", result=result, reasons=reasons)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
 
